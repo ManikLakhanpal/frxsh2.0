469d4405e8187cc5a0ae80a1fd997bdbb7c0cf14
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-11-10 18:53:24 +0530

    shop page was added using router and file structure was made readable

---
+++ src/containers/Home.jsx
@@ -0,0 +1,13 @@
+import TextScene from "../components/Experience";
+import Content from "../components/content";
+
+function Home() {
+  return (
+    <>
+      <TextScene />
+      <Content />
+    </>
+  );
+}
+
+export default Home;
