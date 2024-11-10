7f83998a938c6958b0c4e8e4b45b8f674188291b
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-11-11 02:12:56 +0530

    product card fixed

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -73,7 +73,7 @@ function Context({ children }) {
                 "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/women/1.webp",
             description:
                 "Embellishments at front add class and style to this crewneck, short-sleeve top from Karl Lagerfeld.",
-            price: "₹500.00",
+            price: 500.00
         },
         {
             name: "Custom Womens T Shirts",
@@ -81,7 +81,7 @@ function Context({ children }) {
                 "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/women/customize.webp",
             description:
                 "100% Cotton, Preshrunk Ring Spun Jersey Knit for cool comfort; Seamless double stitched",
-            price: "₹1100",
+            price: 1100
         },
         {
             name: "OVERSIZED ZIPPED SWEATSHIRT",
@@ -89,7 +89,7 @@ function Context({ children }) {
                 "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/women/4.jpg",
             description:
                 "Hooded sweatshirt made of cotton yarn. Long sleeves finished with cuffs. Front metal zip fastening.",
-            price: "₹320",
+            price: 320
         },
         {
             name: "TRF SLIM CROPPED BELL BOTTOM",
@@ -97,7 +97,7 @@ function Context({ children }) {
                 "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/women/5.jpg",
             description:
                 "MID-WAIST - CROPPED BELL BOTTOM. Zip fly and metal top button fastening.",
-            price: "₹280",
+            price: 280
         },
         {
             name: "PLAIN FINE KNIT SWEATER",
@@ -105,7 +105,7 @@ function Context({ children }) {
                 "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/women/6.jpg",
             description:
                 "Basic sweater with a round neck and long sleeves. Side vents at the hem. Ribbed trims.",
-            price: "₹300",
+            price: 300
         },
         {
             name: "SILK BLOUSE WITH FLOWERS",
@@ -113,7 +113,7 @@ function Context({ children }) {
                 "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/women/7.jpg",
             description:
                 "Blouse with flowers in silk fabric. V-neck with hidden buttons and long sleeves.",
-            price: "₹800",
+            price: 800
         },
         {
             name: "RIPPED SKINNY JEANS",
@@ -121,7 +121,7 @@ function Context({ children }) {
                 "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/women/8.jpg",
             description:
                 "Skinny jeans with ripped details and frayed hem. Zip fly and button fastening.",
-            price: "₹450",
+            price: 450
         },
     ];
 
@@ -129,43 +129,43 @@ function Context({ children }) {
         {
             name: "Cohen Loafers",
             description: "full-grain leather",
-            price: "₹500.00",
+            price: 500.00,
             image: "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/sneaker/1.webp",
         },
         {
             name: "Men's Kicks",
             description: "We are committed to sustainability.",
-            price: "₹1100",
+            price: 1100,
             image: "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/sneaker/1.jpg",
         },
         {
             name: "Canvas Shoes",
             description: "All Royalty-Free licences include",
-            price: "₹320",
+            price: 320,
             image: "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/sneaker/4.webp",
         },
         {
             name: "Garcia Sneakers",
             description: "full-grain leather & suede",
-            price: "₹2800",
+            price: 2800,
             image: "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/sneaker/5.webp",
         },
         {
             name: "Reid Sneakers - men's",
             description: "pebbled leather",
-            price: "₹300",
+            price: 300,
             image: "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/sneaker/6.webp",
         },
         {
             name: "BMorgen Trainers",
             description: "full-grain leather & suede",
-            price: "₹350",
+            price: 350,
             image: "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/sneaker/7.jpg",
         },
         {
             name: "Beaumont Loafers",
             description: "water-repellent suede",
-            price: "₹6990",
+            price: 6990,
             image: "https://w16manik.blr1.cdn.digitaloceanspaces.com/assets/sneaker/7.webp",
         },
     ];
