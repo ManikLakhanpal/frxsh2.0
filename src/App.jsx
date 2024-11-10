7f72dbff8585cd4e656b8fe42c4a396e051923dc
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-11-11 01:51:18 +0530

    router fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Shop from "./containers/Shop";
 import { Context } from "./context/Context";
 import CustomCursor from "./components/cursor/Cursor";
 import NavigationBar from "./containers/NavigationBar";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import SignIn from "./containers/SignIn";
 import SignUp from "./containers/SignUp";
 import ContactUs from "./containers/ContactUs";
@@ -12,19 +12,17 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <Context>
-      <Router>
-        <CustomCursor />
-        <NavigationBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* Dynamic Route for Shop Categories */}
-          <Route path="/shop/:category" element={<Shop />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/contact" element={<ContactUs />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <CustomCursor />
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Dynamic Route for Shop Categories */}
+        <Route path="/shop/:category" element={<Shop />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+      <Footer />
     </Context>
   );
 }
