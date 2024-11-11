52e173a552cef8661c1382f01d3070897e1b0b70
adishgarg <gargadi456@gmail.com>
2024-11-11 11:43:02 +0530

    frxsh

--- src/components/cursor/Cursor.jsx
+++ src/components/cursor/Cursor.jsx
@@ -5,10 +5,22 @@ import './CustomCursor.css';
 const CustomCursor = () => {
   const cursorRef = useRef(null);
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [cursorPosition2, setCursorPosition2] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const handleMouseMove = (event) => {
-      setCursorPosition({ x: event.clientX - 10, y: event.clientY -80 });
+      setCursorPosition({ x: event.clientX - 10, y: event.clientY -72 });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setCursorPosition2({ x: event.clientX-12, y: event.clientY-80 });
     };
 
     window.addEventListener('mousemove', handleMouseMove);
@@ -19,14 +31,24 @@ const CustomCursor = () => {
   }, []);
 
   return (
+    <>
     <motion.div
-      className="custom-cursor border-"
+      className="custom-cursor"
       ref={cursorRef}
       animate={{ x: cursorPosition.x, y: cursorPosition.y }}
-      transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+      transition={{ type: 'spring', stiffness: 500, damping: 20 }}
       whileHover={{ scale: 2 }}
       onClick={{}}
     />
+    <motion.div
+      className="custom-cursor2"
+      ref={cursorRef}
+      animate={{ x: cursorPosition2.x, y: cursorPosition2.y }}
+      transition={{ type: 'spring', stiffness: 500, damping:35 }}
+      whileHover={{ scale: 2 }}
+      onClick={{}}
+    />
+    </>
   );
 };
 
